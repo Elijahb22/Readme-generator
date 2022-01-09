3118363c84d8360cf87bca1c48f958455ad936dd
elijahb22 <elibusick11@gmail.com>
2022-01-09 15:08:51 -0500

    added 5 license badges

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,6 +1,24 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (license === 'Apache 2.0') {
+    return `[![License: Apache 2.0](https://img.shields.io/badge/Label-Apache__2.0-brightgreen)]`
+  }
+  if (license === 'ISC') {
+    return `[![License: ISC ](https://img.shields.io/badge/Label-ISC-red)]`
+  }
+  if (license === 'MIT') {
+    return `[![License: MIT](https://img.shields.io/badge/Label-MIT-yellowgreen)]`
+  }
+  if (license === 'ODbL') {
+    return `[![License: ODbL](https://img.shields.io/badge/Label-ODbL-orange)]`
+  }
+  if (license === 'CC0') {
+    return`[[License: CC0](https://img.shields.io/badge/Label-CC0-blue)]`
+  } else {
+    return "";
+  }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string

363622bc0045ed8d1658a66768db527293d1d45a
elijahb22 <elibusick11@gmail.com>
2022-01-09 14:19:32 -0500

    added git user info to questions array

--- index.js
+++ index.js
@@ -2,7 +2,18 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 // TODO: Create an array of questions for user input
-const questions = [];
+const userQuestions = () => {
+    return inquirer.prompt({
+        {
+            type:'input',
+            name:'gituser',
+            message:'Please enter your GitHub username',
+            validate: gitUserInput => {
+
+            }
+        }
+    })
+}
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
